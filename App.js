8ca13cbd9a60a72cc57dd59092f45cba74346389
Franciszek <franiu325@gmail.com>
2025-10-22 23:59:50 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
 
+
   return (
     <View style={styles.container}>
       <Text>Ilość tapnięć:</Text>
